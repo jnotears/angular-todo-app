289dd7674d4f8912b2faf77fb40915843e00dfbb
jnotears <jnotears@gmail.com>
2020-07-26 01:49:45 +0700

    modify: register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: 'register.component.html'
@@ -7,14 +9,36 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
     registerForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder) { }
+    constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthService,
+        private router: Router
+    ) { }
 
     ngOnInit() {
         this.registerForm = this.formBuilder.group({
             firstName: ['', Validators.required],
             lastName: ['', Validators.required],
             username: ['', Validators.required],
-            password: ['', Validators.required, Validators.minLength(6)],
+            password: ['', [Validators.required, Validators.minLength(6)]],
         });
+    }
+
+    get form() {
+        return this.registerForm.controls;
+    }
+
+    onSubmit() {
+        if (this.registerForm.invalid) {
+            return;
+        }
+        this.authService.register(this.registerForm.value).subscribe(
+            data => {
+                this.router.navigate(['login']);
+            },
+            error => {
+
+            }
+        );
     }
 }
