05ab004079184ca03914794da146c24903832780
jnotears <jnotears@gmail.com>
2020-07-26 01:50:54 +0700

    modify: auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { User } from '../models/user';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
-
+    public host = 'http://localhost:3000';
     constructor(
         private http: HttpClient
     ) { }
 
     login(username: string, password: string) {
-        return this.http.post<any>('http://localhost:3000/auth/login', { username, password }).pipe(
+        return this.http.post<any>(this.host + '/auth/login', { username, password }).pipe(
             map(
                 data => {
                     if (data) {
@@ -26,6 +27,22 @@ export class AuthService {
             )
 
         )
+    }
+
+    register(user: User) {
+        return this.http.post<any>(this.host + '/auth/register', user).pipe(
+            map(
+                data => {
+                    if (data) {
+                        localStorage.setItem('uid', data.id);
+                        localStorage.setItem('access_token', data.access_token);
+                        return data;
+                    } else {
+                        return;
+                    }
+                }
+            )
+        );
     }
 
     logout() {
